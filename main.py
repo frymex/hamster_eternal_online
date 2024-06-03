c3f1ca3aba92ab95a7453b19caf512c1bf97dd88
Nikita <nikprotect@gmail.com>
2024-06-03 19:15:44 +0300

    Update to 1.1

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import os
 import time
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from utils.printer import print_cazqev
+
+from config import auth_key, interval
 from hamster import Hamster
 
+
 scheduler = BackgroundScheduler()
-
-auth_key = os.environ.get('HAMSTER_API_KEY', '<api_key>')
-interval = 1  # в часах
-
 h = Hamster(auth_key=auth_key)
 
 if __name__ == '__main__':
+    print_cazqev()
+
     scheduler = BackgroundScheduler()
     scheduler.add_job(h.sync, 'interval', hours=interval)
     scheduler.start()
 
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    try:
+        connected_status = h.is_connected()
+    except UnicodeEncodeError:
+        connected_status = False
+
+    if bool(connected_status) is False:
+        print('~ Не удалось подключиться к Hamster Kombat. Проверьте токен')
+        exit(2)
+
+    print('~ Скрипт успешно запущен. Чтобы его остановить нажми Ctrl+C')
+    print(f'~ Выполнен вход под уч. записью: {connected_status.telegramUser.firstName}')
 
     try:
         while True:
